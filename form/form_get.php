47b75a87d2886e49b5eb0190c627b25107037b98
Frans Guero <antoniirawan798@gmail.com>
2024-12-06 11:03:12 +0700

    perubahan 2

---
+++ form/form_get.php
@@ -0,0 +1,9 @@
+<?php
+echo "BERIKUT DATA ANDA : ";
+echo "<br />";
+
+
+echo "Nama Anda : ", $_GET['nama'];
+echo "<br />";
+echo "Email Anda : ", $_GET['email'];
+?>
---
+++ form/form_post.php
@@ -0,0 +1,9 @@
+<?php
+echo "BERIKUT DATA ANDA : ";
+echo "<br />";
+
+
+echo "Nama Anda : ", $_POST['nama'];
+echo "<br />";
+echo "Email Anda : ", $_POST['email'];
+?>
