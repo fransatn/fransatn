a1729d2fbef32c7f83987b34ee9f8a0d42cd11f1
Frans Guero <antoniirawan798@gmail.com>
2024-12-07 10:24:38 +0700

    form tambahan

--- form/form_post.php
+++ form/form_post.php
@@ -2,7 +2,7 @@
 echo "BERIKUT DATA ANDA : ";
 echo "<br />";
 
-
+// menerima data yang dikirim dari form
 echo "Nama Anda : ", $_POST['nama'];
 echo "<br />";
 echo "Email Anda : ", $_POST['email'];
--- form/form_request.php
+++ form/form_request.php
@@ -3,7 +3,7 @@ echo "BERIKUT DATA ANDA : ";
 echo "<br />";
 
 // menerima data yang dikirim dari form
-echo "Nama Anda : ", $_GET['nama'];
+echo "Nama Anda : ", $_REQUEST['nama'];
 echo "<br />";
-echo "Email Anda : ", $_GET['email'];
+echo "Email Anda : ", $_REQUEST['email'];
 ?>
